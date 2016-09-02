2a143fdeca5458fe993a8250fd81ac226c34d960
Matt Grey <matthewgrey_04@msn.com>
2016-09-02 23:41:53 +0100

    licenses and typography

---
+++ _includes/javascript/menu.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.mobile-nav').on('click', function(){
+    $('.navigation').toggleClass('show');
+    $('.mobile-nav').html(function(i, label){
+      return label === 'Menu' ? '&times; Close' : 'Menu'
+    })
+    $("body").toggleClass("prevent-scroll");
+  });
+});
--- javascript/site.js
+++ javascript/site.js
@@ -1,9 +1,3 @@
-$(document).ready(function() {
-  $('.mobile-nav').on('click', function(){
-    $('.navigation').toggleClass('show');
-    $('.mobile-nav').html(function(i, label){
-      return label === 'Menu' ? '&times; Close' : 'Menu'
-    })
-    $("body").toggleClass("prevent-scroll");
-  });
-});
+---
+---
+{% include /javascript/menu.js %}

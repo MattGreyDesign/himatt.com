8fee35527652da032f17968a262d06f1dd45961c
Matt Grey <matthewgrey_04@msn.com>
2017-02-20 14:23:48 +0000

    cleaned up

--- javascript/site.js
+++ javascript/site.js
@@ -1,3 +1,23 @@
 ---
 ---
 {% include /javascript/menu.js %}
+{% include /javascript/headroom.js %}
+
+$(function () {
+    var header = new Headroom(document.querySelector('[data-header]'),
+    {
+        tolerance: 15,
+        offset: 10,
+        classes: {
+            initial: "fixed",
+            pinned: "slideDown",
+            unpinned: "slideUp",
+
+            top: "top",
+            notTop: "notTop",
+            bottom: "end",
+            notBottom: "notEnd"
+        }
+    });
+    header.init();
+}());

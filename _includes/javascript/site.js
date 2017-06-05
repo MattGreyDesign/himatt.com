a4a81a7a8f87655bef16e49bc2b3224ac55bed22
Matt Grey <matthewgrey_04@msn.com>
2017-06-05 22:08:39 +0100

    optimise font

--- _includes/javascript/site.js
+++ _includes/javascript/site.js
@@ -1,10 +1,11 @@
----
----
 {% include /javascript/menu.js %}
 {% include /javascript/headspace.js %}
 
 $(function () {
   Headspace(document.querySelector('[data-header]'), {
+    startOffset: 90,     // default: height of element
+    tolerance: 5,        // default: 8
+    showAtBottom: false, // default: true
     classNames: {
       base: 'sticky-header',
       fixed: 'sticky-header--fixed',
